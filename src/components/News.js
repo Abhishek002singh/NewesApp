5e0d7a36af1adbb993eec5cc0a0b520a33e33672
Abhishek Singh <newtonabhishek2002@gmal.com>
2024-09-26 00:07:55 +0530

    Initial commit of NewsApp

---
+++ src/components/News.js
@@ -0,0 +1,136 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+export default class News extends Component {
+  static defaultProps = {
+    country: "us",
+    pageSize: 8,
+    category: "science",
+  };
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      loading: true,
+      page: 1,
+      totalResults: 0,
+      hasMore: true
+    };
+    document.title = `${this.capitalizeFirstLetter(
+      this.props.category
+    )} - NewsMonkey`;
+  }
+
+  async updateNews(pageNo) {
+    this.props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    this.props.setProgress(30);
+    let parsedata = await data.json();
+    this.props.setProgress(70);
+    this.setState({
+      articles: parsedata.articles,
+      totalResults: parsedata.totalResults,
+      loading: false,
+    });
+
+    this.props.setProgress(100);
+  }
+
+  async componentDidMount() {
+    this.updateNews();
+  }
+
+  handlePrevClick = async () => {
+    this.setState({ page: this.state.page - 1 });
+    this.updateNews();
+  };
+
+  handleNextClick = async () => {
+    this.setState({ page: this.state.page + 1 });
+    this.updateNews();
+  };
+
+  // fetchMoreData = async () => {
+  //   this.setState({ page: this.state.page + 1 });
+  //   const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=1dad387c96d7442b8b3d423d10031ba6&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    
+  //   let data = await fetch(url);
+  //   let parsedata = await data.json();
+  //   this.setState({
+  //     articles: this.state.articles.concat(parsedata.articles),
+  //     totalResults: parsedata.totalResults
+  //   });
+  // };
+
+
+  fetchMoreData = async () => {
+    if (this.state.articles.length < this.state.totalResults) {
+      this.setState({ page: this.state.page + 1 });
+      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+      
+      let data = await fetch(url);
+      let parsedata = await data.json();
+      this.setState({
+        articles: this.state.articles.concat(parsedata.articles),
+        totalResults: parsedata.totalResults
+      });
+    } else {
+      this.setState({ hasMore: false });
+    }
+  };
+
+  
+
+  render() {
+    return (
+      <>
+        <h1 className="text-center" style={{ margin: "35px 0px" }}>
+          NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)}{" "}
+          Headlines
+        </h1>
+        {/* {this.state.loading && <Spinner/>} */}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.hasMore}
+          loader={<Spinner />}
+        >
+          <div className="container">
+            <div className="row">
+              {this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem
+                      title={element.title ? element.title : ""}
+                      description={element.description ? element.description : ""}
+                      imageUrl={element.urlToImage}
+                      newsUrl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
+      </>
+    );
+  }
+}
